15b2125acd5ef8a24423cf723396decfa88eba5f
Rob Williams <robccwilliams@gmail.com>
2019-04-17 13:59:33 +0100

    Corrected detection and extraction logic, revised the probe_data data structure

--- src/probe_finder.py
+++ src/probe_finder.py
@@ -1,4 +1,5 @@
 from math import ceil
+import os
 
 FASTA_OUTPUT = "output/result.fasta"
 TSV_OUTPUT = "output/result.tsv"
@@ -33,6 +34,7 @@ class ProbeData:
             self.end = source.end
             self.direction = source.direction
             self.matched = False
+            self.printed = False
             if overrides is not None:
                 for attribute, value in overrides.items():
                     setattr(self, attribute, value)
@@ -46,8 +48,9 @@ class ProbeData:
             self.alignment_length = line_tokens[6]
             self.acc_sequence = line_tokens[7]
             self.scaffold_alignment = line_tokens[8]
-            self.frame = line_tokens[9]
+            self.frame = int(line_tokens[9])
             self.matched = False
+            self.printed = False
             first_position = int(line_tokens[3])
             second_position = int(line_tokens[4])
             if first_position < second_position:
@@ -59,8 +62,20 @@ class ProbeData:
                 self.end = first_position
                 self.direction = "N"
 
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __lt__(self, other):
+        return self.scaffold < other.scaffold
+
     def to_fasta(self):
-        return f">{self.scaffold} {self.start} {self.end} {self.direction}\n{self.scaffold_alignment}\n"
+        if self.direction == "P":
+            start = self.start
+            end = self.end
+        else:
+            start = self.end
+            end = self.start
+        return f">{self.scaffold} {start} {end} {self.direction}\n{self.scaffold_alignment}\n"
 
     def to_tsv(self):
         if self.direction == "P":
@@ -90,7 +105,7 @@ class ProbeData:
         }
         if first.end < second.start:
             overrides["scaffold_alignment"] = "".join([first.scaffold_alignment,
-                                                       "N" * ((second.start - first.end) / 3),
+                                                       "N" * ceil((second.start - first.end) / 3),
                                                        second.scaffold_alignment])
         elif first.end > second.start:
             overlap = ceil((first.end - second.start) / 3)  # Account for nucleotide -> protein conversion
@@ -98,35 +113,41 @@ class ProbeData:
                                                        second.scaffold_alignment[:overlap]])
         align_length = len(overrides["scaffold_alignment"]) - str.count("-", overrides["scaffold_alignment"])
         overrides["alignment_length"] = align_length
+        overrides["accession_id"] = first.accession_id + "_" + second.accession_id
         return ProbeData(first, overrides)
 
 
 def read_probe_records_from_file(filename):
-    return_list = []
+    record_list = []
     with open(filename) as probe_in:
         for line in probe_in:
-            return_list.append(ProbeData(line))
-    return return_list
+            record_list.append(ProbeData(line))
+    record_dict = {}
+    for record in record_list:
+        if record.scaffold not in record_dict:
+            record_dict[record.scaffold] = [record]
+        else:
+            record_dict[record.scaffold].append(record)
+    return record_dict
 
 
 # Account for incomplete probe, merge the alignment so that the
 def is_near_neighbour(master, comparitor):
-    print(f"NEAR NEIGHBOUR: master:{master.start}-{master.end} comparitor:{comparitor.start}-{comparitor.end}")
     first_diff = master.start - comparitor.end
     second_diff = comparitor.start - master.end
-    return (0 < first_diff <= 50 or 0 < second_diff <= 50) \
-           and master.direction == comparitor.direction
+    return (0 < first_diff <= 50 or 0 < second_diff <= 50) and master.direction == comparitor.direction and master.frame == comparitor.frame
 
 
 def is_range_extension(master, comparitor):
-    print("RANGE EXTENSION")
-    return (comparitor.start < master.start and master.end < comparitor.end) or \
-           (master.start > comparitor.start and master.end > comparitor.end)
+    return (((master.start <= comparitor.start) and (comparitor.start <= master.end < comparitor.end)) or
+            ((comparitor.start <= master.start) and (master.start <= comparitor.end < master.end)) or
+            ((master.end >= comparitor.end) and (comparitor.start <= master.start < comparitor.end)) or
+            ((comparitor.end >= master.end) and (master.start <=comparitor.start < master.end))) and \
+            master.direction == comparitor.direction and master.frame == comparitor.frame
 
 
 def is_superset(master, comparitor):
-    print("SUPERSET")
-    return master.start > comparitor.start and master.end < comparitor.end
+    return master.start >= comparitor.start and master.end <= comparitor.end and master.frame == comparitor.frame
 
 
 def write_to_files(record):
@@ -136,39 +157,44 @@ def write_to_files(record):
             tsv.write(record.to_tsv())
 
 
-def clear_output_files():
+def set_up_output_files():
     with open(FASTA_OUTPUT, "w"):
         with open(TSV_OUTPUT, "w"):
             pass
 
 
+def unique_scaffolds(source_one, source_two):
+    source_one_uniques = [scaffold for scaffold in source_one.keys() if scaffold not in source_two.keys()]
+    source_two_uniques = [scaffold for scaffold in source_two.keys() if scaffold not in source_one.keys()]
+    return_set = set()
+    for unique in source_one_uniques:
+        return_set.update(source_one[unique])
+        del source_one[unique]
+    for unique in source_two_uniques:
+        return_set.update(source_two[unique])
+        del source_two[unique]
+    return return_set
+
+
 def run():
-    clear_output_files()
+    set_up_output_files()
     first_probe_data = read_probe_records_from_file(INPUT_FILE_ONE)
     second_probe_data = read_probe_records_from_file(INPUT_FILE_TWO)
-    for probe_record in first_probe_data:
-        print("IN FIRST PROBE LOOP")
-        for probe_comparitor in second_probe_data:
-            print("IN SECOND PROBE LOOP")
-            if probe_record.scaffold == probe_comparitor.scaffold:
-                print("SCAFFOLD MATCH")
-                probe_record.matched = True
-                probe_comparitor.matched = True
-                if is_superset(probe_record, probe_comparitor):
-                    print("SUPERSET")
-                    write_to_files(probe_comparitor)
-                elif is_near_neighbour(probe_record, probe_comparitor) \
-                        or is_range_extension(probe_record, probe_comparitor):
-                    print("GOING TO MERGE")
-                    write_to_files(ProbeData.merge_records(probe_record, probe_comparitor))
-                else:
-                    print("NEVERMIND")
-                    continue
-        if not probe_record.matched:
-            print("NOTHING MATCHED")
-            write_to_files(probe_record)
-    unmatched_comparitors = [comparitor for comparitor in second_probe_data if comparitor.matched is False]
-    map(write_to_files, unmatched_comparitors)
+    # Add any scaffolds which don't appear in the other file automatically
+    output_set = unique_scaffolds(first_probe_data, second_probe_data)
+
+    for scaffold, records in first_probe_data.items():
+        for record in records:
+            current_print_candidate = record
+            for comparitor in second_probe_data[scaffold]:
+                if is_superset(current_print_candidate, comparitor):
+                    current_print_candidate = comparitor
+                elif is_near_neighbour(current_print_candidate, comparitor) \
+                        or is_range_extension(current_print_candidate, comparitor):
+                    current_print_candidate = ProbeData.merge_records(current_print_candidate, comparitor)
+            output_set.add(current_print_candidate)
+
+    [write_to_files(output) for output in sorted(list(output_set))]
 
 
 if __name__ == "__main__":
