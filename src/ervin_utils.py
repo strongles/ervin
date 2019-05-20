c9815d5e2bb7c37fb6014f5620f5d5910565b44a
Rob Williams <robccwilliams@gmail.com>
2019-05-20 21:14:14 +0100

    Fix for ridiculous OSX behaviour of converting colons to slashes when saving a filename and therefore impeding the ability to subsequently use any such filename from the commandline

--- src/ervin_utils.py
+++ src/ervin_utils.py
@@ -3,4 +3,4 @@ import datetime
 
 def format_timestamp_for_filename():
     current_timestamp = datetime.datetime.now()
-    return current_timestamp.strftime("%Y-%m-%d_%H:%M:%S")
+    return current_timestamp.strftime("%Y-%m-%d_%H-%M-%S")
