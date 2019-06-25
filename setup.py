8604994feae95db4eb69201085ba2fb9128bd45a
Rob Williams <robccwilliams@gmail.com>
2019-06-25 21:22:55 +0100

    Minor formatting improvements, handling for building new version when on a commit between tags

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 import setuptools
 
-with open('README.md') as readme:
+with open("README.md") as readme:
     long_desc = str(readme.read())
 
 setuptools.setup(
-    name='ervin',
-    version='0.0.3',
-    description='ERVin is a collection of tools developed to assist '
-                'in discovering ERV sequences within genomic data',
+    name="ervin",
+    version="0.0.3",
+    description="ERVin is a collection of tools developed to assist "
+                "in discovering ERV sequences within genomic data",
     long_description=long_desc,
-    long_description_content_type='text/markdown',
-    author='Rob Williams',
-    author_email='robccwilliams@gmail.com',
+    long_description_content_type="text/markdown",
+    author="Rob Williams",
+    author_email="robccwilliams@gmail.com",
     entry_points={
-        'console_scripts': ['ervin = ervin.ervin:run']
+        "console_scripts": ["ervin = ervin.ervin:run"]
     },
-    licence='GPL-3.0-or-later',
-    url='https://github.com/strongles/ervin',
-    download_url='https://github.com/strongles/ervin/archive/0.0.3.tar.gz',
+    licence="GPL-3.0-or-later",
+    url="https://github.com/strongles/ervin",
+    download_url="https://github.com/strongles/ervin/archive/0.0.3.tar.gz",
     packages=setuptools.find_packages(),
     install_requires=[
             "biopython==1.73"
@@ -29,13 +29,13 @@ setuptools.setup(
             "ftputil==3.4"
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Environment :: MacOS X',
-        'Intended Audience :: Science/Research',
-        'Operating System :: MacOS :: MacOS X',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Environment :: MacOS X",
+        "Intended Audience :: Science/Research",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
     ]
 )
--- setup_generate.py
+++ setup_generate.py
@@ -8,47 +8,47 @@ SMALL_INDENT = ' ' * 4
 SEPARATOR = f"{NEWLINE}{BIG_INDENT}"
 SETUP_CONTENT = """import setuptools
 
-with open('README.md') as readme:
+with open("README.md") as readme:
     long_desc = str(readme.read())
 
 setuptools.setup(
-    name='ervin',
-    version='{version}',
-    description='ERVin is a collection of tools developed to assist '
-                'in discovering ERV sequences within genomic data',
+    name="ervin",
+    version="{version}",
+    description="ERVin is a collection of tools developed to assist "
+                "in discovering ERV sequences within genomic data",
     long_description=long_desc,
-    long_description_content_type='text/markdown',
-    author='Rob Williams',
-    author_email='robccwilliams@gmail.com',
+    long_description_content_type="text/markdown",
+    author="Rob Williams",
+    author_email="robccwilliams@gmail.com",
     entry_points={{
-        'console_scripts': ['ervin = ervin.ervin:run']
+        "console_scripts": ["ervin = ervin.ervin:run"]
     }},
-    licence='GPL-3.0-or-later',
-    url='https://github.com/strongles/ervin',
-    download_url='https://github.com/strongles/ervin/archive/{version}.tar.gz',
+    licence="GPL-3.0-or-later",
+    url="https://github.com/strongles/ervin",
+    download_url="https://github.com/strongles/ervin/archive/{version}.tar.gz",
     packages=setuptools.find_packages(),
     install_requires={require_list},
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Environment :: MacOS X',
-        'Intended Audience :: Science/Research',
-        'Operating System :: MacOS :: MacOS X',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Environment :: MacOS X",
+        "Intended Audience :: Science/Research",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
     ]
 )
 """
 
 
 def get_current_git_tag():
-    git_version_binary = subprocess.run(['git', 'describe'], capture_output=True).stdout
-    return git_version_binary.decode('utf-8').strip()
+    git_version_binary = subprocess.run(["git", "describe", "--abbrev=0"], capture_output=True).stdout
+    return git_version_binary.decode("utf-8").strip()
 
 
 def get_requirements():
-    with open('requirements.txt') as requires:
+    with open("requirements.txt") as requires:
         require_list = [f'"{line.strip()}"' for line in requires.readlines()]
     return f"[{NEWLINE}{BIG_INDENT}{SEPARATOR.join(require_list)}{NEWLINE}{SMALL_INDENT}]"
 
